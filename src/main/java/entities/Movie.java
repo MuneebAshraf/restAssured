bab93ae74f50dac0d78f2d04b61cc7eabdb2aaa9
MuneebAshraf <Munne_122@hotmail.com>
2022-09-14 14:59:57 +0200

    foobar

---
+++ src/main/java/entities/Movie.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+
+@Entity
+@NamedQuery(name = "Movie.deleteAllRows", query = "DELETE from Movie")
+public class Movie implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String genre;
+
+    public Movie() {
+    }
+
+    public Movie(String title, String genre)
+    {
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getGenre()
+    {
+        return genre;
+    }
+
+    public void setGenre(String genre)
+    {
+        this.genre = genre;
+    }
+}
